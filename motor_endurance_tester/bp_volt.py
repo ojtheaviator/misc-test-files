662ba31efc86be5dc1c03b34ff846a608ac0c297
ojtheaviator <john2792@purdue.edu>
2024-01-21 13:42:20 -0500

    Update bp_volt.py

--- motor_endurance_tester/bp_volt.py
+++ motor_endurance_tester/bp_volt.py
@@ -1,39 +1,12 @@
-#!/usr/bin/env python
-# All credit to Antonvh:  https://gist.github.com/antonvh/c81c247fc03029a1ba6a
-# View his work here:  https://github.com/antonvh
-##
-#
-# See more about the BrickPi here:  http://www.dexterindustries.com/BrickPi
-#
-# These files have been made available online through a Creative Commons Attribution-ShareAlike 3.0  license.
-# (http://creativecommons.org/licenses/by-sa/3.0/)
-
-#For the code to work - sudo pip install -U future
-
 from __future__ import print_function
 from __future__ import division
 from builtins import input
 
-# the above lines are meant for Python3 compatibility.
-# they force the use of Python3 functionality for print(), 
-# the integer division and input()
-# mind your parentheses!
-
 import smbus
 
 def get_voltage():
-    """
-    Reads the digital output code of the MCP3021 chip on the BrickPi+ over i2c.
-    Some bit operation magic to get a voltage floating number.
 
-    If this doesnt work try this on the command line: i2cdetect -y 1
-    The 1 in there is the bus number, same as in bus = smbus.SMBus(1)
-    Google the resulting error.
-
-    :return: voltage (float)
-    """
-
-    # time.sleep(0.1) # Necessary?
+    time.sleep(0.01) # Necessary?
     try:
             bus = smbus.SMBus(1)            # SMBUS 1 because we're using greater than V1.
             address = 0x48
