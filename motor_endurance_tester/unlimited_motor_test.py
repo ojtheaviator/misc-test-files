15c14f7d41bfa002254f735eb598def46ca2c94a
ojtheaviator <john2792@purdue.edu>
2024-01-21 11:25:09 -0500

    Fixed stuff

---
+++ motor_endurance_tester/unlimited_motor_test.py
@@ -0,0 +1,55 @@
+import brickpi3
+import time
+from bp_volt import get_voltage
+import sys
+import time
+
+#CONFIG:
+power_setting = 100 #%
+refresh_frequency = 2 #s
+#END CONFIG:
+
+try:
+
+    BP = brickpi3.BrickPi3()
+    BP.set_motor_power(BP.PORT_A,0)
+    BP.set_motor_power(BP.PORT_B,0)
+
+    orig_time = time.time()
+    prevenc = 0
+    run = True
+
+    outputStr = f"Encoder A:\tEncoder B:\tBattery Voltage:\tUnix Time:\n"
+    print(outputStr)
+    with open(f"motor_log_{orig_time}.csv", "w") as fid:
+        fid.write(outputStr)
+
+    BP.set_motor_power(BP.PORT_A, power_setting)
+    BP.set_motor_power(BP.PORT_B, power_setting)
+
+
+    while run:
+        encoder_A = BP.get_motor_encoder(BP.PORT_A)
+        encoder_B = BP.get_motor_encoder(BP.PORT_B)
+        
+        bat_volt = get_voltage()
+        if (bat_volt < 6.3): #dangerously low battery level: turn off
+            BP.set_motor_power(BP.PORT_A, 0)
+            BP.set_motor_power(BP.PORT_B, 0)
+            run = False
+        
+        time_elapsed = time.time() - orig_time
+        
+        outputStr = f"{encoder_A}\t{encoder_B}\t{bat_volt}\t{time_elapsed}"
+        print(outputStr)
+        with open(f"motor_log_{orig_time}.csv", "a") as fid:
+            fid.write(outputStr+"\n")
+        
+        time.sleep(refresh_frequency)
+except KeyboardInterrupt:
+    BP.set_motor_power(BP.PORT_A, 0)
+    BP.set_motor_power(BP.PORT_B, 0)
+    sys.exit()
+    
+
+
