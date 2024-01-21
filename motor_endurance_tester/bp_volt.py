da1800792b24226b2f12285fd515a3d997b74250
ojtheaviator <john2792@purdue.edu>
2024-01-21 13:58:03 -0500

    Update bp_volt.py

--- motor_endurance_tester/bp_volt.py
+++ motor_endurance_tester/bp_volt.py
@@ -7,27 +7,28 @@ import smbus
 def get_voltage():
 
     try:
-            bus = smbus.SMBus(1)            # SMBUS 1 because we're using greater than V1.
-            address = 0x48
-            # read data from i2c bus. the 0 command is mandatory for the protocol but not used in this chip.
-            data = bus.read_word_data(address, 0)
+        bus = smbus.SMBus(1)        # SMBUS 1 because we're using greater than V1.
+        address = 0x48
+        # read data from i2c bus. the 0 command is mandatory for the protocol but not used in this chip.
+        data = bus.read_word_data(address, 0)
 
-            # from this data we need the last 4 bits and the first 6.
-            last_4 = data & 0b1111 # using a bit mask
-            first_6 = data >> 10 # left shift 10 because data is 16 bits
+        # from this data we need the last 4 bits and the first 6.
+        last_4 = data & 0b1111 # using a bit mask
+        first_6 = data >> 10 # left shift 10 because data is 16 bits
 
-            # together they make the voltage conversion ratio
-            # to make it all easier the last_4 bits are most significant :S
-            vratio = (last_4 << 6) | first_6
+        # together they make the voltage conversion ratio
+        # to make it all easier the last_4 bits are most significant :S
+        vratio = (last_4 << 6) | first_6
 
-            # Now we can calculate the battery voltage like so:
-            ratio = 0.01818     # this is 0.1/5.5V Still have to find out why...
-            voltage = vratio * ratio
+        # Now we can calculate the battery voltage like so:
+        ratio = 0.01818     # this is 0.1/5.5V Still have to find out why...
+        voltage = vratio * ratio
 
-            return "{:.3F}".format(voltage)
+        return "{:.3F}".format(voltage)
 
-    except:
-            return False
-            
+    except Exception as e:
+        print(e)
+        return None
+
 if __name__ == "__main__":
     print(get_voltage())
